062021f03d6228a24dd15c3f6df8d4fab961695d
julio cesar <juliocmcnha@gmail.com>
2025-12-01 23:44:03 -0300

    Atualização

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const Place = require('./models/Place');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || '';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://187.21.4.20:27017/geoapp';
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
